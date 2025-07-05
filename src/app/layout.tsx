28bc8c9fef37a08de944511d4750fa69781ca3a4
Kyle Aig-Imoukhuede <kyle.o.aig@gmail.com>
2025-07-05 11:54:59 -0400

    adds google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -24,6 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <GoogleAnalytics gaId="G-433K2G324W" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} relative antialiased`}
       >
