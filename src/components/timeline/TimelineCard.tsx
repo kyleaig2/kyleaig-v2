ed73cde4f68f79c74751f55a2d58519c57568dbe
Kyle Aig-Imoukhuede <kyle.o.aig@gmail.com>
2025-07-08 18:37:15 -0400

    styling updates

--- src/components/timeline/TimelineCard.tsx
+++ src/components/timeline/TimelineCard.tsx
@@ -35,8 +35,8 @@ function TimelineCard({
   focused = false,
 }: TimelineCardProps) {
 
-  const tl_point_classes = `${focused ? "after:shadow-lg after:bg-zinc-800" : "after:bg-zinc-400" } first:after:bg-zinc-800 after:transition-all after:duration-500 after:absolute after:w-4 after:h-4 dark:after:bg-zinc-300 after:-left-11.5 after:rotate-45 after:z-2`;
-  const more = `${focused ? "last:before:shadow-lg" : "" } last:before:absolute last:before:w-4 last:before:h-4 last:before:bg-zinc-800 dark:last:before:bg-zinc-300 last:before:-left-11.5 last:before:top-full last:before:rotate-45 last:before-top-5`;
+  const tl_point_classes = `${focused ? "after:shadow-sm after:bg-zinc-800 dark:first:after:bg-zinc-200" : "after:bg-zinc-400 dark:after:bg-zinc-600" } first:after:bg-zinc-800 dark:first:after:bg-zinc-200 after:transition-all after:duration-500 after:absolute after:w-4 after:h-4 dark:after:bg-zinc-300 after:-left-11.5 after:rotate-45 after:z-2`;
+  const more = `${focused ? "last:before:shadow-sm" : "" } last:before:absolute last:before:w-4 last:before:h-4 last:before:bg-zinc-800 dark:last:before:bg-zinc-300 last:before:-left-11.5 last:before:top-full last:before:rotate-45 last:before-top-5`;
 
   const yearInfo = Array.isArray(years) ? `${years[0]} - ${years[1]}` : years;
 
@@ -44,8 +44,8 @@ function TimelineCard({
     <div
       id={id}
       className={`${inter.className} ${
-        focused ? "shadow-lg" : ""
-      } hover:bg-zinc-200/70 dark:hover:bg-zinc-900/80 bg-zinc-200/30 dark:bg-zinc-900/30 hover:shadow-lg transition-all max-w-2xl relative flex flex-col text-sm ${tl_point_classes} ${more}`}
+        focused ? "shadow-lg dark:bg-zinc-900/80 bg-zinc-200/70" : ""
+      } hover:bg-zinc-200/70 dark:hover:bg-zinc-900/80 bg-zinc-200/30 dark:bg-zinc-900/30 hover:shadow-lg transition-all duration-500 max-w-2xl relative flex flex-col text-sm ${tl_point_classes} ${more}`}
     >
       {/* Header*/}
       <div className="flex justify-between text-zinc-600 dark:text-zinc-200 px-7 pt-5 items-start">
